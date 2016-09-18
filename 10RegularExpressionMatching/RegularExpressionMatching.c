be95d2951e5ab0e065f9e36d5cf308ede045cfc8
zhangzheng <kezy-5566@163.com>
2016-09-18 19:40:49 +0800

    update 10 RegularExpressionMatching

--- 10RegularExpressionMatching/RegularExpressionMatching.c
+++ 10RegularExpressionMatching/RegularExpressionMatching.c
@@ -1,5 +1,5 @@
 /*
- *create by xiao
+ *create by zhangzheng 
  *email : kezy-5566@163.com
  *time : 2016-5-23
  *Regular Expression Matching:
@@ -12,6 +12,7 @@
  *	bool isMatch(const char *s, const char *p)
  *
  *	Some examples:
+ *	isMatch("","aa") -> false
  *	isMatch("aa","a") -> false
  *	isMatch("aa","aa") -> true
  *	isMatch("aaa","aa") -> false
@@ -23,11 +24,44 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 bool isMatch(char *s, char *p)
 {
-	int len = strlen(s);
-	for (int i = 0; i < len; i++) {
-		
+	if (*p == '\0') 
+		return *s == '\0';
+	
+	//当下一个字符不是*号时，需要字符匹配
+	if (*(p+1) != '*') {
+		if (*s != '\0' && (*p == '.' || *s == *p))
+			return isMatch(s+1, p+1);
+		else 
+			return false;
+	} else {
+		while (*s != '\0' && (*p == '.' || *s == *p)){
+		//后续还需要比较，所以需要判断之后是否匹配，不匹配则跳到下个字符
+			if (isMatch(s, p+2)) 
+				return true;
+			s++;
+		}	
+		return isMatch(s, p+2);
 	}
+}
+
+
+int main(int args, char **argv)
+{
+	char *s = "aad";
+	char *p = "aa.*";
+
+	if (args > 2) {
+		s = argv[1];
+		p = argv[2];
+	}
+
+	if (isMatch(s, p))
+		printf("%s and %s matching True\n", s, p);
+	else
+		printf("%s and %s matching False\n", s, p);
+
 }
