e525cf0b1770451b7a49f6b58130b2c6953b8efa
zhangzheng <kezy-5566@163.com>
2017-08-03 16:55:44 +0800

    回溯法解决生成括号字符串问题

--- solution/src/xyz/vimtool/Main.java
+++ solution/src/xyz/vimtool/Main.java
@@ -1,21 +1,14 @@
 package xyz.vimtool;
 
-import xyz.vimtool.commons.ListNode;
-import xyz.vimtool.problem21.Solution;
+import xyz.vimtool.problem22.Solution;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode node1 = new ListNode(1);
-        node1.next = new ListNode(4);
-
-        ListNode node2 = new ListNode(2);
-        node2.next = new ListNode(3);
-
-        ListNode listNode = solution.mergeTwoLists(node1, node2);
-        System.out.println(1);
-        System.out.println(3 & 0x1);
-        System.out.println(2 & 0x1);
+        List<String> list = solution.generateParenthesis(3);
+        System.out.println("");
     }
 }
--- solution/src/xyz/vimtool/problem22/Solution.java
+++ solution/src/xyz/vimtool/problem22/Solution.java
@@ -1,5 +1,8 @@
 package xyz.vimtool.problem22;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 22. Generate Parentheses
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
@@ -12,9 +15,32 @@ package xyz.vimtool.problem22;
  * "()()()"
  * ]
  *
+ * 使用回溯法，每次添加一个括号，一直递归得到字符串
+ *
  * @author zhangzheng
  * @version 1.0.0
  * @date 2017-8-1
  */
 public class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "(", 1, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max) {
+            backtrack(list, str+"(", open+1, close, max);
+        }
+        if(close < open) {
+            backtrack(list, str+")", open, close+1, max);
+        }
+    }
 }
